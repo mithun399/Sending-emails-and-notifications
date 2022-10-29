516646672c2da2f867630d6138e5cf2ee3757919
MS <msovik399@gmail.com>
2022-10-30 01:15:59 +0600

    send email multiple user using facade and create custom template for sending email

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use  Illuminate\Support\Facades\Auth;
 use App\Notifications\emailNotification;
 use App\Models\User;
+use Illuminate\Support\Facades\Notification;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,10 +20,17 @@ use App\Models\User;
 Route::get('/', function () {
     return view('welcome'); 
 });
-
+//single user
+// Route::get('send', function () {
+//     $user=User::find(1);
+//     $user->notify(new emailNotification());
+//     return redirect()->back();
+// });
+//multiple user
 Route::get('send', function () {
-    $user=User::find(1);
-    $user->notify(new emailNotification());
+    $user=User::all();
+    Notification::send($user, new emailNotification());
+    
     return redirect()->back();
 });
 
