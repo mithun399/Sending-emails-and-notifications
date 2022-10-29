63591496653acb79a1659bda458d69e93fc0541c
MS <msovik399@gmail.com>
2022-10-30 01:57:33 +0600

    implement queue

--- app/Notifications/emailNotification.php
+++ app/Notifications/emailNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class emailNotification extends Notification
+class emailNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
